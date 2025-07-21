1a26b2d230c7afff65516be5ef3c98af76fa0d13
Mann Patel <patelmann2705@gmail.com>
2025-07-22 00:31:27 +0530

    final

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+var sidemenu = document.getElementById("sidemenu");
+function openMenu(){
+    sidemenu.style.right = "0";
+}
+function closeMenu(){
+    sidemenu.style.right = "-200px";
+}
+
+// Navigation stays same - no scroll changes
+
